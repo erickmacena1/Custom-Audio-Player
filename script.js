17a6b49bf1711d78389b3b62199f8fac71b4cc9f
ErickMacena <erickmacena2@gmail.com>
2021-04-08 22:45:31 -0300

    First commit!!

---
+++ script.js
@@ -0,0 +1,53 @@
+
+function start()
+{
+    const music = document.getElementById("mcPozeDoRodo")
+    const buttons = Array.from(document.getElementsByClassName("audioBtn"))
+    let [decrease, increase, play, stop, volume] = buttons
+    let audioCountdown = document.getElementsByClassName("audioTime")[0]
+    const musicDuration = music.duration
+
+
+    music.volume = 0.6
+
+    play.addEventListener("click", playBtnClick)
+    stop.addEventListener("click", stopBtnClick)
+
+    window.alert("AINDA TA EM CONSTRUÇÃO MIZERA. Mas aceito criticas :)")
+}
+
+function playBtnClick()
+{
+    if(play.dataset.state == 0)
+    {
+        music.play()
+        play.src = "assets/images/070-pause button.svg"
+        play.dataset.state = 1
+    }
+    else
+    {
+        music.pause()
+        play.src = "assets/images/072-play button.svg"
+        play.dataset.state = 0
+    }
+}
+
+function stopBtnClick()
+{
+    if(play.dataset.state == 1)
+    {
+        music.pause()
+        play.src = "assets/images/072-play button.svg"
+        play.dataset.state = 0
+        music.currentTime = 0
+    }
+    
+}
+
+function howThisWorks()
+{
+    while(audioCountdown.dataset.counting == "true")
+    {
+        console.log("Está tocando")
+    }
+}
